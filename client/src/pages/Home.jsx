04b7309aa65514a1f7a9f8cd6ba512ce8c382b85
Donny <107092000+Donny-Guo@users.noreply.github.com>
2026-04-21 15:55:05 -0400

    home page top section (WIP)

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import "../styles/Home.css"
+import Logo from "../assets/meal.png"
 const Home = () => {
     return (
-        <div>
-            <h1>This is Home Page</h1>
+        <div className="home-page">
+            <header className="header-section">
+                <div className="left-section">
+                    <img src={Logo} alt="logo.png" className="logo-image"/>
+                    <h2 className="app-title">
+                        PotluckHub
+                    </h2>
+                </div>
+
+                <div className="right-section">
+                    <button>
+                        Log in
+                    </button>
+
+                    <button>
+                        Sign up
+                    </button>
+                </div>
+            </header>
+
+            <div>
+
+            </div>
         </div>
     )
 }
